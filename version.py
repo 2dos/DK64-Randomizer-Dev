f241d97107f3429fa415b53ef14dc6a5af3274e9
Killklli <tvatter@hotmail.com>
2023-12-17 06:30:57 +0000

    Automated release: 2023-12-17, 06:30:57

--- version.py
+++ version.py
@@ -1,31 +1,4 @@
 """Holds the version for DK64 Rando."""
-import js
+version = "2.1.0"
 
-stable_version = "2.0"
-dev_version = "2.1"
-
-try:
-    url = js.location.hostname.lower().replace("/", "").replace("http:", "").replace("https:", "")
-except Exception:
-    url = "localhost"
-
-only_version = "0.0"
-current_version = "DK64R 0.0"
-if url == "dk64randomizer.com":
-    current_version = "DK64 Randomizer v" + stable_version
-    only_version = stable_version
-else:
-    current_version = "DK64R Dev v" + dev_version
-    only_version = dev_version
-
-try:
-    js.document.title = current_version
-except Exception:
-    pass
-try:
-    js.document.getElementById("live-version").text = current_version + " | "
-except Exception:
-    pass
-whl_hash = "no_file_using_filler_hash"
-
-whl_hash = "e37f57f36b6a2612ce1ba5efd4d49403"
+whl_hash = "dc29aebd6503620c8a2c9972e1b59d44"
